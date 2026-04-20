7b69e370c11f3493f383134c8c414498c2d657de
Haino <haikoshiro2025@gmail.com>
2026-04-20 14:38:01 +0300

    Добавление модели Chapter и List внутри Book

--- src/Avalon.Domain/Entities/Books/Book.cs
+++ src/Avalon.Domain/Entities/Books/Book.cs
@@ -6,12 +6,12 @@ public class Book
 
     public required string Title {get; set;}
 
-    public string? Text {get; set;}
-
     public string? Description {get; set;}
 
     public string? Dedication {get; set;}
 
     public DateTimeOffset CreatedAt {get; set;}
+
+    public List<Chapter> Chapters {get; set;} = new();
 
 }
--- src/Avalon.Domain/Entities/Books/Chapter.cs
+++ src/Avalon.Domain/Entities/Books/Chapter.cs
@@ -1,2 +1,8 @@
 namespace Avalon.Domain.Entities.Books;
 
+public class Chapter
+{
+    public string Title {get; set;}
+
+    public string Text {get; set;}
+}
